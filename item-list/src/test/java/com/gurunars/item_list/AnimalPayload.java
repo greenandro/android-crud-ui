114af69ce6c72e92a20e3922b6c9164ca7efeb84
Anton Berezin <gurunars@gmail.com>
2017-03-11 17:39:21 +0200

    ff

---
+++ item-list/src/test/java/com/gurunars/item_list/AnimalPayload.java
@@ -0,0 +1,31 @@
+package com.gurunars.item_list;
+
+
+class AnimalPayload implements Payload {
+
+    private int version;
+    private Type type;
+
+    @Override
+    public Enum getType() {
+        return type;
+    }
+
+    enum Type {
+        MONKEY, TIGER, WOLF, LION
+    }
+
+    public void update() {
+        this.version++;
+    }
+
+    public AnimalPayload(int version, Type type) {
+        this.version = version;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "" + type + " @ " + version;
+    }
+}
