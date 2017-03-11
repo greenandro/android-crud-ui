01ec82446ff11d1e4ce6a1c5988aa60e9aa14e8b
Anton Berezin <gurunars@gmail.com>
2017-03-11 15:41:53 +0200

    ff

--- item-list/src/main/java/com/gurunars/item_list/SelectableItemList.java
+++ item-list/src/main/java/com/gurunars/item_list/SelectableItemList.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import butterknife.ButterKnife;
 
 public class SelectableItemList<PayloadType extends Payload> extends FrameLayout {
 
-    private ItemList<ConcreteSelectablePayload<PayloadType>> itemList;
+    private ItemList<SelectablePayload<PayloadType>> itemList;
+
+    private List<Item<PayloadType>> items = new ArrayList<>();
+    private Set<Item<PayloadType>> selectedItems = new HashSet<>();
 
     public SelectableItemList(Context context) {
         this(context, null);
@@ -22,5 +30,34 @@ public class SelectableItemList<PayloadType extends Payload> extends FrameLayout
         super(context, attrs, defStyleAttr);
         inflate(context, R.layout.selectable_item_list, this);
         itemList = ButterKnife.findById(this, R.id.itemList);
+
+
+    }
+
+    private void updateItemList() {
+        List<Item<SelectablePayload<PayloadType>>> selectableItems = new ArrayList<>();
+        for (Item<PayloadType> item: items) {
+            selectableItems.add(new Item<>(item.getId(), new SelectablePayload<>(item.getPayload())));
+        }
+        itemList.setItems(selectableItems);
+    }
+
+    public void setItems(List<Item<PayloadType>> items) {
+        this.items = items;
+        updateItemList();
+    }
+
+    public void setSelection(Set<Item<PayloadType>> selectedItems) {
+        this.selectedItems = selectedItems;
+        updateItemList();
+    }
+
+    public void registerItemViewBinder(Enum itemType,
+                                       ItemViewBinder<Item<SelectablePayload<PayloadType>>> itemViewBinder) {
+        itemList.registerItemViewBinder(itemType, itemViewBinder);
+    }
+
+    public void setEmptyViewBinder(EmptyViewBinder emptyViewBinder) {
+        itemList.setEmptyViewBinder(emptyViewBinder);
     }
 }
--- item-list/src/main/java/com/gurunars/item_list/SelectablePayload.java
+++ item-list/src/main/java/com/gurunars/item_list/SelectablePayload.java
@@ -1,5 +1,42 @@
 package com.gurunars.item_list;
 
-public interface SelectablePayload extends Payload {
-    boolean isSelected();
+final class SelectablePayload<PayloadType extends Payload> implements Payload {
+
+    private final PayloadType payload;
+    private boolean selected = false;
+
+    SelectablePayload(PayloadType payload) {
+        this.payload = payload;
+    }
+
+    public final boolean isSelected() {
+        return selected;
+    }
+
+    public PayloadType getPayload() {
+        return payload;
+    }
+
+    void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    @Override
+    public int hashCode() {
+        return payload.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SelectablePayload) {
+            SelectablePayload other = (SelectablePayload) obj;
+            return payload.equals(other.payload) && selected == other.selected;
+        }
+        return false;
+    }
+
+    @Override
+    public Enum getType() {
+        return payload.getType();
+    }
 }
